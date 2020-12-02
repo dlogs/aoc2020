46727dd36dc1aa1c0a07967983d31202eed4c703
David Logsdon <davidlogsdon0@gmail.com>
2020-12-02 08:14:28 -0500

    day 2

--- util/input.ts
+++ util/input.ts
@@ -1,7 +1,14 @@
 import { readFile } from 'fs';
-export function readLinesAsIntArray(file: string, callback: (nums: number[]) => any) {
+
+export function readLinesAsArray(file: string, callback: (lines: string[]) => any) {
   readFile(file, 'utf8', (err, data) => {
     if (err) throw err;
-    callback(data.split('\n').map(s => parseInt(s, 10)))
+    callback(data.split('\n').filter(s => s.length > 0))
   });
+}
+
+export function readLinesAsIntArray(file: string, callback: (nums: number[]) => any) {
+  readLinesAsArray(file, lines => {
+    callback(lines.map(s => parseInt(s, 10)))
+  })
 }
