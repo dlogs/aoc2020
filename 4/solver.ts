19b74fc2d5f66c43e91f7da0e4d2abab057e5ece
David Logsdon <davidlogsdon0@gmail.com>
2020-12-04 18:27:18 -0500

    cleanup day 4

--- 4/solver.ts
+++ 4/solver.ts
@@ -9,32 +9,32 @@ readLinesAsArrayByBlanks("4/input.txt", lines => {
   }
 })
 
-function isValidPassport1(line: string) {
-  const requiredFields = [
-    'byr', // (Birth Year)
-    'iyr', // (Issue Year)
-    'eyr', // (Expiration Year)
-    'hgt', // (Height)
-    'hcl', // (Hair Color)
-    'ecl', // (Eye Color)
-    'pid', // (Passport ID)
-    //'cid', // (Country ID)
-  ]
+const requiredFields = [
+  'byr', // (Birth Year)
+  'iyr', // (Issue Year)
+  'eyr', // (Expiration Year)
+  'hgt', // (Height)
+  'hcl', // (Hair Color)
+  'ecl', // (Eye Color)
+  'pid', // (Passport ID)
+  //'cid', // (Country ID)
+]
 
+function isValidPassport1(line: string) {
   return requiredFields.every(f => line.includes(f))
 }
 
-function isValidPassport2(line: string) {
-  const validations: [string, (x: string) => boolean][] = [
-    ['byr', (x: string) => stringBetween(x, 1920, 2002)],
-    ['iyr', (x: string) => stringBetween(x, 2010, 2020)],
-    ['eyr', (x: string) => stringBetween(x, 2020, 2030)],
-    ['hgt', (x: string) => x.endsWith("cm") ? stringBetween(x, 150, 193) : x.endsWith("in") ? stringBetween(x, 59, 76) : false],
-    ['hcl', (x: string) => !!x.match(/^#[0-9a-f]{6}$/)],
-    ['ecl', (x: string) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth',].includes(x)],
-    ['pid', (x: string) => !!x.match(/^[0-9]{9}$/)]
-  ]
+const validations: [string, (x: string) => boolean][] = [
+  ['byr', x => stringBetween(x, 1920, 2002)],
+  ['iyr', x => stringBetween(x, 2010, 2020)],
+  ['eyr', x => stringBetween(x, 2020, 2030)],
+  ['hgt', x => x.endsWith("cm") ? stringBetween(x, 150, 193) : x.endsWith("in") ? stringBetween(x, 59, 76) : false],
+  ['hcl', x => !!x.match(/^#[0-9a-f]{6}$/)],
+  ['ecl', x => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth',].includes(x)],
+  ['pid', x => !!x.match(/^[0-9]{9}$/)]
+]
 
+function isValidPassport2(line: string) {
   const parts = line.split(' ').map(field => field.split(':')).reduce((hash, value) => hash.set(value[0], value[1]), new Map<string, string>())
   const result =  validations.every(v => parts.get(v[0]) && v[1](parts.get(v[0])!))
   // if (isValidPassport1(line)) {
