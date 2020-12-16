a59da99b97de389452d06cd5d0216770ed710a21
David Logsdon <davidlogsdon0@gmail.com>
2020-12-16 15:27:10 -0500

    fix day 16

--- 16/solver.ts
+++ 16/solver.ts
@@ -1,4 +1,4 @@
-import { sum } from "lodash";
+import { sortBy, sum } from "lodash";
 import { asArray } from "../util/input";
 import part from "../util/part";
 
@@ -23,18 +23,27 @@ asArray(lines => {
   if (part == 1) {
     console.warn(sum(nearbyTickets.flatMap(t => t).filter(n => !rules.some(r => r.matches(n)))))
   } else {
-    const validTickets = nearbyTickets.filter(t => t.some(n => rules.some(r => r.matches(n))))
-    const possibleRules = myTicket.map(n => rules.filter(r => r.matches(n)))
-    console.info(possibleRules)
+    const validTickets = nearbyTickets.filter(t => t.every(n => rules.some(r => r.matches(n))))
+    let possibleRules = myTicket.map(n => rules.filter(r => r.matches(n)))
     for (const ticket of validTickets) {
       for (let i = 0; i < possibleRules.length; i++) {
         possibleRules[i] = possibleRules[i].filter(r => r.matches(ticket[i]))
       }
     }
-    console.info(possibleRules)
+    const sortedRules = sortBy(possibleRules, rs => rs.length)
+    for (let i = 0; i < sortedRules.length; i++) {
+      const onlyRule = sortedRules[i][0];
+      for (let j = i + 1; j < sortedRules.length; j++) {
+        const rules = sortedRules[j]
+        rules.splice(rules.indexOf(onlyRule), 1)
+      }
+    }
+    console.log(possibleRules)
+
     let product = 1
     for (let i = 0; i < possibleRules.length; i++) {
       if (possibleRules[i][0].name.startsWith("departure")) {
+        console.log(`including ${possibleRules[i][0].name}: ${myTicket[i]}`)
         product *= myTicket[i]
       }
     }
